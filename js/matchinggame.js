b1940bd9b7df93b5a46c7aa8284522017856f419
tamaslud <tamaslud@yahoo.com>
2022-09-01 21:54:36 +0200

    random set of cards flipped down

--- js/matchinggame.js
+++ js/matchinggame.js
@@ -1,25 +1,59 @@
 'use strict';
-/*
 
-let cards = [
-    {1:{
-        name:'1',
-        visible: true,
-        selectable : true,
-        faceImage : ""
-    }},
+const numberOfCards = 10;
 
+const cardsImages = {
+    0: './css/img/ace_of_spades.png',
+    1: './css/img/2_of_diamonds.png',
+    2: './css/img/3_of_clubs.png',
+    3: './css/img/4_of_hearts.png',
+    4: './css/img/5_of_spades.png',
+    5: './css/img/ace_of_spades.png',
+    6: './css/img/2_of_diamonds.png',
+    7: './css/img/3_of_clubs.png',
+    8: './css/img/4_of_hearts.png',
+    9: './css/img/5_of_spades.png',
+};
 
-];
-*/
+let cardArray = [0,1,2,3,4,5,6,7,8,9];
+
+const randomizeCards = () => {
+    for (let index = 0; index < 50; index++) {
+        const a = Math.floor(Math.random() * numberOfCards);
+        const b = Math.floor(Math.random() * numberOfCards);
+        [cardArray[a], cardArray [b]] = [cardArray[b], cardArray [a]];
+    }
+};
+
+randomizeCards();
+
+let templateCardBox;
+
+const boardContainer = document.querySelector('.board');
+
+for (let i = 0; i < 10; i++) {
+    const cardPath = cardsImages[cardArray[i]];
+    templateCardBox = `
+    <div class="card-box">
+    <div class="card">
+        <div class="card-front">
+            <img src="${cardPath}" width="100%" height="100%" alt="">
+        </div>
+        <div class="card-back">
+            <img src="./css/img/card_backside.jpg" width="100%" height="100%" alt="">
+        </div>
+    </div>`
+
+    boardContainer.innerHTML += (templateCardBox);
+}
+
 let countCards = 0;
 
-const cardT = document.querySelectorAll('.card');
-
+const allCards = document.querySelectorAll('.card');
 
 const addCardListener = () => {
-    for (let i = 0; i < cardT.length; i++) {
-        cardT[i].addEventListener("click", () => turncard(i));
+    for (let i = 0; i < allCards.length; i++) {
+        allCards[i].addEventListener("click", () => turncard(i));
     }
 
 
@@ -30,7 +64,7 @@ const turncard = (index) => {
     if (countCards == 2) {
         undefined;
     } else {
-        cardT[index].style.setProperty('transform', 'rotateY(180deg)');
+        allCards[index].style.setProperty('transform', 'rotateY(180deg)');
     }
 
 
