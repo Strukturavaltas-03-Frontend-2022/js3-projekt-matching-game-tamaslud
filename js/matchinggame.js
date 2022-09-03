ae00e326e17469ffde8fc2bcb84838aeb2017212
tamaslud <tamaslud@yahoo.com>
2022-09-03 14:23:16 +0200

    game ok, timers mising

--- js/matchinggame.js
+++ js/matchinggame.js
@@ -4,11 +4,13 @@ const numberOfCards = 10;
 
 const cardsImages = {
     backside : './css/img/card_backside.jpg',
+    //
     0: './css/img/ace_of_spades.png',
     1: './css/img/2_of_diamonds.png',
     2: './css/img/3_of_clubs.png',
     3: './css/img/4_of_hearts.png',
     4: './css/img/5_of_spades.png',
+    //
     5: './css/img/ace_of_spades.png',
     6: './css/img/2_of_diamonds.png',
     7: './css/img/3_of_clubs.png',
@@ -17,7 +19,7 @@ const cardsImages = {
 };
 
 let cardArray = [0,1,2,3,4,5,6,7,8,9];
-let countCards = 0;
+let turnedCards = [];
 
 const randomizeCards = () => {
     for (let index = 0; index < 50; index++) {
@@ -53,20 +55,46 @@ const addCardListener = () => {
 };
 const turncard = (index) => {
     const allCards = document.querySelectorAll('.card');
-    countCards +=1;
-    console.log(countCards);
+    turnedCards.push(index);
     checkFinshedGame();
     allCards[index].style.setProperty('transform', 'rotateY(180deg)');
     allCards[index].classList.add('inactive')
+    setTimeout(() => {checkLastPair()}, 500);
     
 };
 const checkFinshedGame = () => {
-    countCards === numberOfCards ? console.log('FINISHED'): {};
+    turnedCards.length === numberOfCards ? console.log('FINISHED'): {};
     }
 
+const checkLastPair = () => {
+
+    if (turnedCards.length % 2 === 0) {
+        const lastPair = turnedCards.slice(-2);
+        const isPair = (Math.abs(lastPair[0] - lastPair [1]) === numberOfCards/2);
+        console.log(isPair);
+        if (!isPair){
+            const allCards = document.querySelectorAll('.card');
+            for (let index = 0; index < 2; index++) {
+                const lastCard = turnedCards.pop();
+                allCards[lastCard].style.setProperty('transform', 'rotateY(0deg)');
+                allCards[lastCard].classList.remove('inactive')
+            }
+            
+        }
+    }
+
+    
+}
+
+const allowClick = () => {
+    document.querySelector('body').classList.remove('inactive');
+}
+const banClick = () => {
+    document.querySelector('body').classList.add('inactive');
+}
 
 const initialize = () => {
-    randomizeCards();
+    //randomizeCards();
     cardsToHTML(); 
     addCardListener();   
 };
